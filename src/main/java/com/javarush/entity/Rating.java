66ca6c6799346f470680c051db981ca15d2314bb
Bering-l <slava.listerman@gmail.com>
2023-10-26 18:59:16 +0300

    add entity

---
+++ src/main/java/com/javarush/entity/Rating.java
@@ -0,0 +1,15 @@
+package com.javarush.entity;
+
+public enum Rating {
+    G("G"),
+    PG("PG"),
+    PG13("PG-13"),
+    R("R"),
+    NC17("NC-17");
+
+    private final String rating;
+
+    Rating(String rating) {
+        this.rating = rating;
+    }
+}
